1bb6fadfb956e7be0a90efee6472fc60f8f07055
aaapham128 <40476604+aaapham128@users.noreply.github.com>
2018-10-15 21:09:58 -0700

    Backup Comp Files

--- main.js
+++ main.js
@@ -2,10 +2,23 @@ alert("Hey! This is an alert box.");
 
 var myButton = document.querySelector("button");
 var myHeading = document.querySelector("h1");
+var image = document.querySelectorAll("img").item(4);
+
+function Username() {
+  var name = prompt("What is your name?");
+}
 
 myButton.onclick = function() {
   var name = prompt("What is your name?");
-  localStorage.setItem("name:", name);
-  myHeading.innerHTML("name", name);
-  onclick(name);
+  document.getElementById("myButton").innerHTML = name + "'s Bio";
+}
+
+image.onclick = function() {
+    var mySrc = image.getAttribute("src");
+    if (image.src == "bts.jpg") {
+      image.setAttribute(mySrc ="vixx.jpg");
+    }
+    else {
+      image.setAttribute(mySrc = "bts.jpg");
+    }
 }
